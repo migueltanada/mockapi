acfb39cd96b3f36c908d2981a6714f29cc52df8d
migueltanada <miguel.g.tanada@accenture.com>
2021-08-23 14:00:58 +0800

    Added annotations for swagger documentation

--- src/main/java/com/rest/sample/mockapi/MockController.java
+++ src/main/java/com/rest/sample/mockapi/MockController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 // import java.net.URI;
 // import java.util.List;
 
@@ -23,15 +25,15 @@ public class MockController {
     @Autowired
     private MockService service;
 
- 
+    @Operation(summary = "Mock by json body", description = "Send whatever is in path and request body. Path field is always included in the response!")
     @PostMapping("/mock/path/{path}")
-    public MockResponseStatic mockResponse(@RequestBody MockRequest mock){
+    public MockResponseStatic mockResponse(@RequestBody MockRequest mock, @PathVariable String path){
         
         
         MockResponseStatic mockResponse = new MockResponseStatic(
             mock.getMockCode(),
             mock.getMockDescription(),
-            ServletUriComponentsBuilder.fromCurrentRequest().toUriString(),
+            String.format("%s/%s",ServletUriComponentsBuilder.fromCurrentRequest().toUriString(),path),
             mock.getMockStatus(),
             mock.getBody()
         );
@@ -40,6 +42,7 @@ public class MockController {
         return mockResponse;
     }
 
+    @Operation(summary = "Mock by reading json from config file", description = "Send whatever is in application.properties file. select the property using request path. Path field is always included in the respones, unless it is available in application.propreties!")
     @GetMapping("/mock/config/{path}")
     public MockResponseDynamic mockResponse(@PathVariable String path) throws JsonMappingException, JsonProcessingException{
                     
